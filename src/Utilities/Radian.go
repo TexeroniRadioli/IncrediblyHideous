dddc55eb9f16c1434dbd29923b927b320b2e7b11
will.hollingsworth <will.hollingsworth@everykey.com>
2018-04-12 23:09:52 -0400

    Updated radians
    Radians now follow better practices

--- src/Utilities/Radian.go
+++ src/Utilities/Radian.go
@@ -1,4 +1,56 @@
 package Utilities
 
-// Definition of Radian (alias of float64)
-type Radian float64
+import "math"
+
+// Definition of radian struct
+type radian struct {
+	value float64
+}
+
+// Definition of Radian interface
+type Radian interface {
+	Value() float64
+	Equals(radian) bool
+	Add(radian) radian
+	Subtract(radian) radian
+	MultiplyBy(float64) radian
+	DivideBy(float64) radian
+}
+
+// Factory function that creates a radian from a float64 value
+func MakeRadian(initialValue float64) radian {
+	tpi := math.Pi * 2
+	// Ensure that our Radian struct has a value between 0 and 2pi
+	for initialValue < 0 {
+		initialValue += tpi
+	}
+	for initialValue >= tpi {
+		initialValue -= tpi
+	}
+
+	return radian {initialValue}
+}
+
+func (r radian) Value() float64 {
+	return r.value
+}
+
+func (r radian) Equals(o radian) bool {
+	return r.value == o.value
+}
+
+func (r radian) Add(op radian) radian {
+	return MakeRadian(r.value + op.value)
+}
+
+func (r radian) Subtract(op radian) radian {
+	return MakeRadian(r.value - op.value)
+}
+
+func (r radian) MultiplyBy(op float64) radian {
+	return MakeRadian(r.value * op)
+}
+
+func (r radian) DivideBy(op float64) radian {
+	return MakeRadian(r.value / op)
+}
